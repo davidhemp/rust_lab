ff0ebf6c68b24111c32d7bf108c5c5b22acd3467
David Hempston <cyber_veio@hotmail.com>
2025-03-28 13:37:22 +0000

    Random input instead of user input

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,15 +1,13 @@
-use std::io;
 
 fn main() {
+
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Please input your guess.");
 
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    let guess = rand::rng().random_range(1..=100);
 
     println!("You guessed: {}", guess);
+    
 }
