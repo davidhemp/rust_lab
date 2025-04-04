5f565bf720101ae01519ff1b063e7fcd9e29a5e0
David Hempston <cyber_veio@hotmail.com>
2025-04-04 14:07:03 +0000

    Make z mutable

--- dot-product/src/main.rs
+++ dot-product/src/main.rs
@@ -3,7 +3,7 @@ use rand::Rng;
 fn main() {
     let x = [1, 2, 3, 4, 5];
     let y = [6, 7, 8, 9, 10];
-    let z = [0, 0, 0, 0, 0];
+    let mut z = [0, 0, 0, 0, 0];
     for i in 0..5 {
         z[i] = x[i] * y[i];
     }
