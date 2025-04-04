eec8a83f1e6ac87bff85b2d49150c2751a27f3cc
David Hempston <cyber_veio@hotmail.com>
2025-04-04 14:50:28 +0000

    Manual example

--- cross-product/src/main.rs
+++ cross-product/src/main.rs
@@ -1,12 +1,16 @@
 // Description: This program computes the cross product of two vectors in 3D space.
 
 fn main() {
-    let x = [1, 2, 3, 4, 5];
-    let y = [6, 7, 8, 9, 10];
-    let mut z = [[0; 5]; 5];
-    for i in 0..5 {
-        for j in 0..5 {
-            println!("z[{}]: {}", i, z[i][j]);
-        }
+    let x = [1, 2, 3];
+    let y = [4, 5, 6];
+    let mut z = [0; 3];
+
+    // Cross product formula: z = x × y
+    // Manaul map for 3 element arrays
+    z[0] = x[1] * y[2] - x[2] * y[1];
+    z[1] = x[0] * y[2] - x[2] * y[0];
+    z[2] = x[0] * y[1] - x[1] * y[0];
+    for i in 0..3 {
+        println!("z[{}] = {}", i, z[i]);
     }
 }
