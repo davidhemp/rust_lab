9bad473d88424ba0c82aaf6040919c283bafe5cc
David Hempston <cyber_veio@hotmail.com>
2025-04-04 14:05:28 +0000

    Simple iter product

--- dot-product/src/main.rs
+++ dot-product/src/main.rs
@@ -1,5 +1,11 @@
 use rand::Rng;
 
 fn main() {
-    
+    let x = [1, 2, 3, 4, 5];
+    let y = [6, 7, 8, 9, 10];
+    let z = [0, 0, 0, 0, 0];
+    for i in 0..5 {
+        z[i] = x[i] * y[i];
+    }
+    println!("The dot product is: {}", z.iter().sum::<i32>());
 }
