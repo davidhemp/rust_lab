e2453536a7cd8f18dcb014e51bfad790ac91db32
David Hempston <cyber_veio@hotmail.com>
2025-04-04 14:33:53 +0000

    Print out z

--- cross-product/src/main.rs
+++ cross-product/src/main.rs
@@ -3,5 +3,10 @@
 fn main() {
     let x = [1, 2, 3, 4, 5];
     let y = [6, 7, 8, 9, 10];
-    
+    let mut z = [[0; 5]; 5];
+    for i in 0..5 {
+        for j in 0..5 {
+            println!("z[{}]: {}", i, z[i][j]);
+        }
+    }
 }
