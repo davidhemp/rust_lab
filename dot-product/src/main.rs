90ea6f017488a34206664d220c08f581b0a5412e
David Hempston <cyber_veio@hotmail.com>
2025-04-04 14:09:32 +0000

    Move to a function

--- dot-product/src/main.rs
+++ dot-product/src/main.rs
@@ -1,11 +1,16 @@
 use rand::Rng;
 
+fn dot_product(x: &[i32], y: &[i32]) -> i32 {
+    let mut result = 0;
+    for i in 0..x.len() {
+        result += x[i] * y[i];
+    }
+    result
+}
+
 fn main() {
     let x = [1, 2, 3, 4, 5];
     let y = [6, 7, 8, 9, 10];
-    let mut z = [0, 0, 0, 0, 0];
-    for i in 0..5 {
-        z[i] = x[i] * y[i];
-    }
-    println!("The dot product is: {}", z.iter().sum::<i32>());
+
+    println!("The dot product is: {}", dot_product(&x, &y));
 }
